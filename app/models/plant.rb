72757c8485d30c8ad2bed8c1c3a771f7b4f0a128
Annie <anniehwang428@gmail.com>
2023-03-20 15:20:41 -0400

    add associations

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,4 @@
 class Plant < ApplicationRecord
+  has_many :schedules
+  has_many :users, through: :schedules
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :schedules
+  has_many :plants, through: :schedules
 end
