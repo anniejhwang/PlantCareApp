3aca0d067ac9a7fa9b20b61cf8cfd5df710f05ed
Annie <anniehwang428@gmail.com>
2023-05-09 14:20:53 -0400

    uncomments routes get schedules

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   post "/sessions" => "sessions#create"
 
   get "/plants" => "plants#index"
-  # get "/schedules" => "schedules#index"
-  # post "/schedules" => "schedules#create"
+  get "/schedules" => "schedules#index"
+  post "/schedules" => "schedules#create"
 end
--- test/controllers/plants_controller_test.rb
+++ test/controllers/plants_controller_test.rb
@@ -4,7 +4,6 @@ class PlantsControllerTest < ActionDispatch::IntegrationTest
   test "index" do
     get "/plants.json"
     assert_response 200
-
     data = JSON.parse(response.body)
     assert_equal Plant.count, data.length
   end
